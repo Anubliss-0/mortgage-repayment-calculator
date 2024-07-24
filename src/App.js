8631dbd6b25c4b8d1f1d064d3bdcf2c193a6daf4
Thomas Rice <59601109+Anubliss-0@users.noreply.github.com>
2024-07-24 17:48:31 +0200

    chore: sass & variables created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/typography.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-preset-1">
+          Here is some text yo!
         </p>
         <a
           className="App-link"
