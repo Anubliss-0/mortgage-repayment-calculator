15c3919f292250c6030f87fa843a6e115236422a
Thomas Rice <59601109+Anubliss-0@users.noreply.github.com>
2024-07-26 17:28:38 +0200

    fix: leaky styles

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,21 @@
 // import React from 'react';
 import NumberFieldPrefix from '../NumberFieldPrefix/NumberFieldPrefix';
 import NumberFieldSuffix from '../NumberFieldSuffix/NumberFieldSuffix';
-import './Form.module.scss'
+import styles from './Form.module.scss'
 
 function Form({ value, onSquareClick }) {
     return (
         <>
-            <NumberFieldPrefix
-                prefixText={"Prefix"}
-                label={'Amount'}
-            />
-            <NumberFieldSuffix
-                suffixText={"Suffix"}
-                label={'Amount'}
-            />
+            <form className={styles.form}>
+                <NumberFieldPrefix
+                    prefixText={"Prefix"}
+                    label={'Amount'}
+                />
+                <NumberFieldSuffix
+                    suffixText={"Suffix"}
+                    label={'Amount'}
+                />
+            </form>
         </>
     );
 }
