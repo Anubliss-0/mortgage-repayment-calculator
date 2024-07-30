22d01adefd035a08c17415a862dd2e0332dbbf06
Thomas Rice <59601109+Anubliss-0@users.noreply.github.com>
2024-07-30 13:01:12 +0200

    added results show logic

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { useState } from 'react';
 function App() {
   const [monthlyRepayments, setMonthlyRepayments] = useState();
   const [total, setTotal] = useState();
+  const [showResults, setShowResults] = useState(false);
 
   const calculateResults = (formData) => {
     const newTotal = calculateTotalRepayment(formData.mortgageAmount, formData.mortgageTerm, formData.interestRate, formData.mortgageType)
     const newMonthlyRepayments = ((newTotal / formData.mortgageTerm) / 12).toFixed(2);
     setTotal(newTotal)
     setMonthlyRepayments(newMonthlyRepayments)
+    setShowResults(true)
   }
 
   function calculateTotalRepayment(mortgageAmount, mortgageTerm, interestRate, mortgageType) {
@@ -19,17 +21,17 @@ function App() {
     const numberOfPayments = mortgageTerm * 12;
 
     if (mortgageType === "repayment") {
-        const monthlyPayment = mortgageAmount * 
-            (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / 
-            (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+      const monthlyPayment = mortgageAmount *
+        (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
+        (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 
-        const totalRepayment = monthlyPayment * numberOfPayments;
-        return totalRepayment.toFixed(2);
+      const totalRepayment = monthlyPayment * numberOfPayments;
+      return totalRepayment.toFixed(2);
     } else if (mortgageType === "interest only") {
       const totalInterest = mortgageAmount * monthlyInterestRate * numberOfPayments;
       return parseFloat(totalInterest).toFixed(2);
+    }
   }
-}
 
   return (
     <div className="App">
@@ -39,6 +41,7 @@ function App() {
       <Results
         monthlyRepayments={monthlyRepayments}
         total={total}
+        showResults={showResults}
       />
     </div>
   );
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,10 +1,37 @@
-function Results({ monthlyRepayments, total }) {
+function Results({ monthlyRepayments, total, showResults }) {
+
     return (
-        <div>
-            <span>{monthlyRepayments}</span>
-            <br></br>
-            <span>{total}</span>
-        </div>
+        <section>
+            {showResults ? (
+                <div>
+                    <header>
+                        <h2>Your Results</h2>
+                        <p>
+                            Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.
+                        </p>
+                    </header>
+                    <div>
+                        <div>
+                            <span>Your monthly repayments</span>
+                            <span>{monthlyRepayments}</span>
+                        </div>
+                        <div>
+                            <span>Total you'll repay over the term</span>
+                            <span>{total}</span>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <header>
+                        <h2>Results shown here</h2>
+                        <p>
+                            Complete the form and click “calculate repayments” to see what your monthly repayments would be.
+                        </p>
+                    </header>
+                </div>
+            )}
+        </section>
     );
 }
 
